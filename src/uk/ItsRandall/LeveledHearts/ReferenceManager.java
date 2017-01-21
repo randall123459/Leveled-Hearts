7390cc6ce275ca96bec89e2d247bdb0d5e45ea75
randall123459 <ryan.randall123459@gmail.com>
2017-01-21 21:39:24 +0000

    Update ReferenceManager.java

--- src/uk/ItsRandall/LeveledHearts/ReferenceManager.java
+++ src/uk/ItsRandall/LeveledHearts/ReferenceManager.java
@@ -2,9 +2,6 @@ package uk.ItsRandall.LeveledHearts;
 
 import org.bukkit.ChatColor;
 
-/**
- * Created by randa on 19/01/2017.
- */
 public class ReferenceManager {
 
     public static final String prefix = ChatColor.AQUA + "Leveled Hearts " + ChatColor.GRAY + "» ";
